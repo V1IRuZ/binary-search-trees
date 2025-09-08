b088985fa694e16df7ed94cb928fbdafa4006feb
Esa <esa-pitkanen@hotmail.com>
2025-09-08 17:01:07 +0300

    Add a Tree class and sort the array in the buildTree method
    The binarytree class accepts an array and builds the tree on the root property. So far, the buildTree method has sorted the given array, which also removes duplicates. The next step is to add the logic to build the tree from the sorted array.

--- bst.js
+++ bst.js
@@ -5,3 +5,22 @@ class Node {
     this.right = null;
   }
 }
+
+export default class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+    const sortedArray = array.toSorted((a, b) => a - b);
+    const uniques = [];
+
+    for (let i = 0; i < sortedArray.length; i++) {
+      if (i === 0 || sortedArray[i] !== sortedArray[i - 1]) {
+        uniques.push(sortedArray[i]);
+      }
+    }
+
+    return uniques;
+  }
+}
