edc94f9ecac7e800c91326a07b543f7b4a4bf7f3
Esa <esa-pitkanen@hotmail.com>
2025-09-16 10:40:41 +0300

    Add a function that creates a random array

--- utils.js
+++ utils.js
@@ -11,4 +11,19 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-export { prettyPrint }
+const randomArrayGenerator = (arrayLength) => {
+  if (arrayLength < 5 || arrayLength > 100) {
+    throw new Error("Array size must be between 5 - 100")
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    result.push(randomNum);
+  }
+
+  return result;
+};
+
+export { prettyPrint, randomArrayGenerator };
