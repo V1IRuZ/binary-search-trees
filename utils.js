a9523427484c88d36d3db544131b20392324cef3
Esa <esa-pitkanen@hotmail.com>
2025-09-16 12:32:42 +0300

    Create a driver script
    The driver script creates the tree, prints out all elements in level, pre, post, and in order, checks the balance, unbalances, and rebalances the tree.

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import Tree from "./bst.js";
+
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -13,7 +15,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 const randomArrayGenerator = (arrayLength) => {
   if (arrayLength < 5 || arrayLength > 100) {
-    throw new Error("Array size must be between 5 - 100")
+    throw new Error("Array size must be between 5 - 100");
   }
 
   const result = [];
@@ -26,4 +28,64 @@ const randomArrayGenerator = (arrayLength) => {
   return result;
 };
 
-export { prettyPrint, randomArrayGenerator };
+const printTraversals = (tree) => {
+  const levelOrder = [];
+  tree.levelOrderForEach((node) => {
+    levelOrder.push(node.data);
+  });
+
+  console.log(`Levelorder: ${levelOrder.join(" ")}`);
+
+  const preOrder = [];
+  tree.preOrderForEach((node) => {
+    preOrder.push(node.data);
+  });
+
+  console.log(`Preorder: ${preOrder.join(" ")}`);
+
+  const inOrder = [];
+  tree.inOrderForEach((node) => {
+    inOrder.push(node.data);
+  });
+
+  console.log(`Inorder: ${inOrder.join(" ")}`);
+
+  const postOrder = [];
+
+  tree.postOrderForEach((node) => {
+    postOrder.push(node.data);
+  });
+
+  console.log(`Postorder: ${postOrder.join(" ")}`);
+};
+
+export default function runTests() {
+  const array = randomArrayGenerator(15);
+  const binarySearchTree = new Tree(array);
+
+  console.log(prettyPrint(binarySearchTree.root));
+
+  console.log(`Tree is balanced: ${binarySearchTree.isBalanced()}`);
+
+  printTraversals(binarySearchTree);
+
+  console.log(
+    "Unbalance the tree, adding the following values ​​to the tree: 120, 125, 150, 200, 240."
+  );
+
+  binarySearchTree.insert(120);
+  binarySearchTree.insert(125);
+  binarySearchTree.insert(150);
+  binarySearchTree.insert(200);
+  binarySearchTree.insert(240);
+
+  console.log(prettyPrint(binarySearchTree.root));
+  console.log(`Tree is balanced: ${binarySearchTree.isBalanced()}`);
+
+  console.log("Rebalancing the tree...");
+  binarySearchTree.rebalance();
+  console.log(prettyPrint(binarySearchTree.root));
+  console.log(`Tree is balanced: ${binarySearchTree.isBalanced()}`);
+
+  printTraversals(binarySearchTree);
+}
